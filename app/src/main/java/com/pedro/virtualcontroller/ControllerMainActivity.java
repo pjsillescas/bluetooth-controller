e43227dedeca46233e5f8a971e8a28045edea15b
Pedro <pjsillescas@gmail.com>
2018-04-13 21:54:34 +0200

    Blank project.

---
+++ app/src/main/java/com/pedro/virtualcontroller/ControllerMainActivity.java
@@ -0,0 +1,13 @@
+package com.pedro.virtualcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControllerMainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_controller_main);
+	}
+}
